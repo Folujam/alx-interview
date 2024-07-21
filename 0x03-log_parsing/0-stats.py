c820cb28799a7be4f461ebd535d9d43b860f4da4
folujam <olusway@gmail.com>
2024-07-21 15:43:24 +0100

    refactored

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,17 +1,48 @@
 #!/usr/bin/python3
 """Log parsing Module"""
 import sys
+#  this docd out script, was initial idea. dosent work yet
+"""import re
 
 
-#  script reads from stdin line by line (p.u. readline())
-#  with sys.stdin.readline():
-#  if
+#  script reads from stdin line by line
+total_size = 0
+status_counts = 0
+"""
+#  for line in sys.stdin:
+#   pattern = r"^(?P<ip>\S+) - \[(?P<date>.+)\] \
+#        'GET /projects/260 HTTP/1.1' (?P<status>\d{3}) (?P<size>\d+)$"
+"""    match = re.match(pattern, line)
+
+    if match:
+        ip = match.group('ip')
+        date = match.group('date')
+        status = match.group('status')
+        size = match.group('size')
+
+    total_size += int(size)
+    status_counts[status] = status_counts.get(status, 0) + 1
+
+    try:
+        for i, line in enumerate(sys.stdin, 1):
+            # process line
+            if i % 10 == 0:
+                print('Total file size:', total_size)
+                for status, count in sorted(status_counts.items()):
+                    print(status, ':', count)
+    except KeyboardInterrupt:
+        print('Interrupted')
+
 #  if line != format skip
 #  after 10lines/signal(ctrl+C) print
 #  total file size = all read file sizes sumed
 #  implement status code (must be integer) -
 #  <sc>: <num> (printed ascd order)
+"""
+
+
 def print_msg(dict_sc, total_file_size):
+    """prints dict message"""
     print("File size: {}".format(total_file_size))
     for key, val in sorted(dict_sc.items()):
         if val != 0:
