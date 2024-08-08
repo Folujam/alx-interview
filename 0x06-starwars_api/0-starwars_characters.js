4caa7cf1fe32384a625ebc2b46d91a934656343a
folujam <olusway@gmail.com>
2024-08-08 23:32:29 +0100

    test #1

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, body) {
+  if (err) throw err;
+  const charactars = JSON.parse(body).characters;
+  exactOrder(charactars, 0);
+});
+// define exactOrder fxn
+const exactOrder = (characters, x) => {
+  if (x === characters.length) return;
+  request(characters[x], function (err, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(characters, x + 1);
+  });
+};
