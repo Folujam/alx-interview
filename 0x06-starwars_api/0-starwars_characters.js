1f766321071e6ec6e95fb9fd7c27a5deabbe68d7
folujam <olusway@gmail.com>
2024-08-08 23:49:57 +0100

    test#5

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, body) {
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
   if (err) throw err;
   const charactars = JSON.parse(body).characters;
   exactOrder(charactars, 0);
@@ -10,7 +10,7 @@ request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err,
 // define exactOrder fxn that gets xters,print 1 on 1line
 const exactOrder = (characters, x) => {
   if (x === characters.length) return;
-  request(characters[x], function (err, body) {
+  request(characters[x], function (err, res, body) {
     if (err) throw err;
     console.log(JSON.parse(body).name);
     exactOrder(characters, x + 1);
