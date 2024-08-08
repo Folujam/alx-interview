f9b798873d3476b5ddb5da7cb1384cd4290c8ef5
folujam <olusway@gmail.com>
2024-08-08 23:44:43 +0100

    chmod and docd

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,13 +1,15 @@
 #!/usr/bin/node
 
+//use request Module
 const request = require('request');
 
+//connect to star-wars api
 request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, body) {
   if (err) throw err;
   const charactars = JSON.parse(body).characters;
   exactOrder(charactars, 0);
 });
-// define exactOrder fxn
+// define exactOrder fxn that gets xters and print 1 on 1line
 const exactOrder = (characters, x) => {
   if (x === characters.length) return;
   request(characters[x], function (err, body) {
